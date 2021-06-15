6219ad778ce791381b6797e17d4639fe2cfaae74
underwolf <matheusozzy1@hotmail.com>
2021-06-15 16:40:22 -0300

    Made Character changing UI

---
+++ Assets/Scripts/ChangePlayerUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePlayerUI : MonoBehaviour
+{
+    public Sprite[] characters;
+    public Image characterActive, nextCharacter;
+
+
+    public void SetCharacter(int CharacterSelected,int NextCharacter)
+    {
+        characterActive.sprite = characters[CharacterSelected];
+        nextCharacter.sprite = characters[NextCharacter];
+    }
+}
